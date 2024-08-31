884a9f6fb713802b6c3d83afc4102ab9debd521a
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-08-31 16:31:24 -0300

    chore(llm_api): update config

--- llm_api/config.py
+++ llm_api/config.py
@@ -9,5 +9,6 @@ URLS = [
     "https://tip.golang.org/tour/concurrency.article",
     "https://tip.golang.org/doc/effective_go",
     # "https://socrates.acadiau.ca/courses/engl/rcunningham/resources/Shpe/Hamlet.pdf",
-    # "https://gosafir.com/mag/wp-content/uploads/2019/12/Tolkien-J.-The-lord-of-the-rings-HarperCollins-ebooks-2010.pdf"
+    "https://gosafir.com/mag/wp-content/uploads/2019/12/Tolkien-J.-The-lord-of-the-rings-HarperCollins-ebooks-2010.pdf"
+    "https://gist.github.com/silver-xu/1dcceaa14c4f0253d9637d4811948437",
 ]
